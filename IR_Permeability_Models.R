8511e8bd4bf3b945841b8735c240c835e2e30a07
alyssaaelizaa <agarci59@gmu.edu>
2025-02-28 16:07:02 -0500

    Loading tecator data.

---
+++ IR_Permeability_Models.R
@@ -0,0 +1,16 @@
+### Exercise 6.1:
+
+# (a) Start R and use these commands to load the data:
+library(caret)
+data(tecator) # use ?tecator to see more details
+
+# (b) In this example the predictors are the measurements at the individual
+# frequencies. Because the frequencies lie in a systematic order (850–1,050 nm),
+# the predictors have a high degree of correlation. Hence, the data lie in a
+# smaller dimension than the total number of predictors (215). Use PCA
+# to determine the effective dimension of these data. What is the effective
+# dimension?
+
+### TODO: 
+# Exercise 6.1: (b) - (e)
+# Exercise 6.2: (a) - (d)
